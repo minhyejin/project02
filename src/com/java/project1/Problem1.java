1eeaacd3178f2ff38d16d12c3b6bed5964af6716
BIT-USER <BIT-USER@DESKTOP-VEEGU4M>
2018-01-08 19:33:43 +0900

    1st

---
+++ src/com/java/project1/Problem1.java
@@ -0,0 +1,20 @@
+package com.java.project1;
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+		int [] data= {1,3,5,8,9,11,15,19,18,20,30,33,31};
+		int sum =0;
+		int count =0;
+		
+		for(int i=0; i<data.length; i++) 
+			if(data[i]%3==0) {
+			count ++;
+			sum = sum+data[i];
+			
+		}
+                System.out.println("3의 배수의 합:"+sum);
+				System.out.println("3의 배수의 개수"+count);
+	}
+
+}
